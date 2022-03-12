aa8a36cff4c6b69477d03726c2b3497489c39d69
hugosenari <hugosenari@gmail.com>
2022-03-12 04:25:03 -0300

    chore: re index nimbench

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Micro benchmarking tool to measure speed of code, with the goal of optimizing it.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimbench-master.flake = false;
+  inputs.src-nimbench-master.owner = "ivankoster";
+  inputs.src-nimbench-master.ref   = "refs/heads/master";
+  inputs.src-nimbench-master.repo  = "nimbench";
+  inputs.src-nimbench-master.type  = "github";
+  
+  inputs."strfmt".dir   = "nimpkgs/s/strfmt";
+  inputs."strfmt".owner = "riinr";
+  inputs."strfmt".ref   = "flake-pinning";
+  inputs."strfmt".repo  = "flake-nimble";
+  inputs."strfmt".type  = "github";
+  inputs."strfmt".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."strfmt".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimbench-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimbench-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
